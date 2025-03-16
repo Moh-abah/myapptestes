861b9c286ad89471b5fe28386e3ea3836175c0c0
Moh abbh <musst92@gmail.com>
2025-03-16 04:47:52 +0300

    Remove consumers.py and channels configuration

--- ein_backend/urls.py
+++ ein_backend/urls.py
@@ -1,14 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from messaging import routing
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         path('messaging/', include('messaging.urls')),
-        path('messaging/ws/', include(routing.websocket_urlpatterns)),
+       
         path('stores/', include('stores.urls')),
         path('reviews/', include('reviews.urls')),
         path('users/', include('users.urls')),
